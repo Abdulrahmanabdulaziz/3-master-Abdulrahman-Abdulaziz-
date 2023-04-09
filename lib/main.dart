cd60ae3259648c375d19d95974554bf2a2ef09ef
Abdulrahman abdulaziz <aa5186@fayoum.edu.eg>
2023-04-09 21:02:59 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home:BusinessAppCard(),
+      //home: BusinessCardApp()
+      home:  BusinessCardAppPage()
+    );
+  }
+}
+
+
